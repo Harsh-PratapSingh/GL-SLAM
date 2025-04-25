f14b2559e1dfbff494fb3432709ecb1900b6b886
Harsh Pratap Singh <harsh681825@gmail.com>
2025-04-25 20:21:37 +0530

    first stable commit :)

--- include/visualization/visualization.h
+++ include/visualization/visualization.h
@@ -4,13 +4,11 @@
 #include <vector>
 
 namespace slam_visualization {
-    void initialize_pangolin();
-
     void draw_frustum(float scale, float r, float g, float b);
 
     void visualize_poses(const std::vector<cv::Mat>& Rs_est, const std::vector<cv::Mat>& Ts_est,
                         const std::vector<cv::Mat>& Rs_gt, const std::vector<cv::Mat>& Ts_gt,
-                        const std::vector<Point3D>& points3D, bool interactive = false);
+                        const std::vector<Point3D>& points3D);
 
     void visualize_optical_flow(const cv::Mat& img_current,
                                const std::vector<cv::Point2f>& points_prev,
